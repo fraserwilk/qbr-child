589903d1bf778f1ac0e45b83b344594cd1bcee49
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2025-07-22 16:02:45 +1000

    add HubTiger integration

--- template-parts/section-hubtiger.php
+++ template-parts/section-hubtiger.php
@@ -2,8 +2,21 @@
 <section class="hubtiger py-5">
   <div class="container">
     <h2 class="row">Book Your Repair</h2>
-    <div class="subheading">Insert HubTiger iFrame here</div>
-    
+    <div class="subheading text-center"> <?php // Centered the div for better button placement ?>
+
+      <?php 
+      // Use the 'Link' field from ACF
+      $link = get_field('hubtiger_button');
+      if( $link ): 
+          $link_url = $link['url'];
+          $link_title = $link['title'];
+          $link_target = $link['target'] ? $link['target'] : '_self';
+          ?>
+          <a class="btn btn-primary btn-lg" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+              <?php echo esc_html( $link_title ); ?>
+          </a>
+      <?php endif; ?>
+      
     </div>
   </div>
 </section>
